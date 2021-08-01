63f62181e89fe7e81ea37a9b2a418909f77bcbe0
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2021-08-01 19:05:05 -0400

    add module egg_tf_user_permissions

---
+++ modules/egg_tf_user_permissions/variables.tf
@@ -0,0 +1,14 @@
+variable "namespace" {
+  description = "string to use for unique naming"
+  type        = string
+}
+
+variable "tf_state_role_arn" {
+  description = "terraform state role arn"
+  type        = string
+}
+
+variable "user_names" {
+  description = "list of user names"
+  type        = list(string)
+}
