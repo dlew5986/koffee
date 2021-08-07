3ea343b3bd1b93603874658366ed2cc5c592a3f2
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2021-08-07 17:58:55 -0400

    add tf_provisioner to user_permissions module

--- modules/egg_tf_user_permissions/variables.tf
+++ modules/egg_tf_user_permissions/variables.tf
@@ -8,6 +8,11 @@ variable "tf_state_role_arn" {
   type        = string
 }
 
+variable "tf_provisioner_role_arn" {
+  description = "terraform provisioner role arn"
+  type        = string
+}
+
 variable "user_names" {
   description = "list of user names"
   type        = list(string)
