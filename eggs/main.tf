31323cb7ae010698c9db6a80d90f383dcf53d4ee
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2021-08-07 17:59:35 -0400

    add egg_tf_provisioner to eggs

--- eggs/main.tf
+++ eggs/main.tf
@@ -32,9 +32,16 @@ module "egg_tf_state" {
   principal_arns = [aws_iam_user.automation.arn]
 }
 
+module "egg_tf_provisioner" {
+  source         = "../modules/egg_tf_provisioner"
+  namespace      = "koffee"
+  principal_arns = [aws_iam_user.automation.arn]
+}
+
 module "egg_tf_user_permissions" {
-  source            = "../modules/egg_tf_user_permissions"
-  namespace         = "koffee"
-  tf_state_role_arn = module.egg_tf_state.config.role_arn
-  user_names        = [aws_iam_user.automation.name]
+  source                  = "../modules/egg_tf_user_permissions"
+  namespace               = "koffee"
+  tf_state_role_arn       = module.egg_tf_state.config.role_arn
+  tf_provisioner_role_arn = module.egg_tf_provisioner.config.role_arn
+  user_names              = [aws_iam_user.automation.name]
 }
--- eggs/outputs.tf
+++ eggs/outputs.tf
@@ -1,3 +1,7 @@
 output "backend_config" {
   value = module.egg_tf_state.config
 }
+
+output "provisioning_config" {
+  value = module.egg_tf_provisioner.config
+}
