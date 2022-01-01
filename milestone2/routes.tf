e6949b43dc11206251911a3d273fa4a447464b29
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-01 14:25:18 -0500

    add route table associations

--- milestone2/routes.tf
+++ milestone2/routes.tf
@@ -3,3 +3,18 @@ resource "aws_route" "default_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
+
+resource "aws_route_table_association" "publicA" {
+  subnet_id = aws_subnet.public_publicA.id
+  route_table_id = aws_default_route_table.rt.id
+}
+
+resource "aws_route_table_association" "publicB" {
+  subnet_id = aws_subnet.public_publicB.id
+  route_table_id = aws_default_route_table.rt.id
+}
+
+resource "aws_route_table_association" "publicC" {
+  subnet_id = aws_subnet.public_publicC.id
+  route_table_id = aws_default_route_table.rt.id
+}
