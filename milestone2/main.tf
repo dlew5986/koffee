cd8c823c34b7b605b9f7041983dd60f4daccefe1
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-01 13:45:12 -0500

    add default tags to aws provider

--- milestone2/main.tf
+++ milestone2/main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region = "us-east-2"
+
+  default_tags {
+    tags = module.tags.all_the_tags
+  }
+
+}
+
+module "tags" {
+  source = "../modules/tags"
+  additional_tags = {
+    github_repo = "koffee"
+  }
 }
