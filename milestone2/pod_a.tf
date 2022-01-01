f045b885b83ec008f269b469ab811070db72c1f5
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-01 18:52:32 -0500

    consolidate pod declarations

--- milestone2/pod_a.tf
+++
@@ -1,9 +0,0 @@
-module "pod_a" {
-  source                 = "./modules/pod"
-  availability_zone_name = data.aws_availability_zone.a.name
-  cidr_block_public      = "172.16.1.0/24"
-  cidr_block_app         = "172.16.4.0/24"
-  cidr_block_db          = "172.16.8.0/24"
-  namespace              = local.namespace
-  vpc_id                 = aws_vpc.vpc.id
-}
--- milestone2/pod_b.tf
+++
@@ -1,9 +0,0 @@
-module "pod_b" {
-  source                 = "./modules/pod"
-  availability_zone_name = data.aws_availability_zone.b.name
-  cidr_block_public      = "172.16.2.0/24"
-  cidr_block_app         = "172.16.5.0/24"
-  cidr_block_db          = "172.16.9.0/24"
-  namespace              = local.namespace
-  vpc_id                 = aws_vpc.vpc.id
-}
