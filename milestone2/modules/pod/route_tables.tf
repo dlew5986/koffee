9e16fbcc1fb7bd87d56aebb78817f8592268c325
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-01 19:10:26 -0500

    add private route table

--- milestone2/modules/pod/route_tables.tf
+++ milestone2/modules/pod/route_tables.tf
@@ -1,3 +1,8 @@
 data "aws_route_table" "main" {
   route_table_id = data.aws_vpc.vpc.main_route_table_id
 }
+
+resource "aws_route_table" "private" {
+  vpc_id = data.aws_vpc.vpc.id
+  tags   = { Name = local.route_table_name_private }
+}
