1eef256ab7c6c1c77c0773910e269830d15b95bc
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-01 19:10:26 -0500

    add private route table

--- milestone2/modules/pod/locals.tf
+++ milestone2/modules/pod/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  subnet_name_public      = lower("${var.namespace}_${var.availability_zone_name}_public")
-  subnet_name_private_app = lower("${var.namespace}_${var.availability_zone_name}_app")
-  subnet_name_private_db  = lower("${var.namespace}_${var.availability_zone_name}_db")
+  subnet_name_public       = lower("${var.namespace}_${var.availability_zone_name}_public")
+  subnet_name_private_app  = lower("${var.namespace}_${var.availability_zone_name}_app")
+  subnet_name_private_db   = lower("${var.namespace}_${var.availability_zone_name}_db")
+  route_table_name_private = lower("${var.namespace}")
 }
