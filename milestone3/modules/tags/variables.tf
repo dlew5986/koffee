37f01d5fb5883b84d71d26e65cea0add942e2e8b
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-03 17:40:38 -0500

    add tags for milestone 3

---
+++ milestone3/modules/tags/variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+  type = string
+}
+
+variable "created_using" {
+  type    = string
+  default = "terraform"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "additional_tags" {
+  type    = map(string)
+  default = {}
+}
