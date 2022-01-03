82d4776fe945c50393d7f2efc5bc1c377d44bad7
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-02 20:03:07 -0500

    fix linting

--- milestone2/key_pair.tf
+++ milestone2/key_pair.tf
@@ -9,19 +9,19 @@ resource "aws_key_pair" "key_pair" {
 }
 
 resource "local_file" "private_key_in_pem_format" {
-  sensitive_content    = tls_private_key.key_pair.private_key_pem
-  filename             = "${path.module}/${local.namespace}.private.pem"
-  file_permission      = "0400"
+  sensitive_content = tls_private_key.key_pair.private_key_pem
+  filename          = "${path.module}/${local.namespace}.private.pem"
+  file_permission   = "0400"
 }
 
 resource "local_file" "public_key_in_pem_format" {
-  sensitive_content    = tls_private_key.key_pair.public_key_pem
-  filename             = "${path.module}/${local.namespace}.public.pem"
-  file_permission      = "0400"
+  sensitive_content = tls_private_key.key_pair.public_key_pem
+  filename          = "${path.module}/${local.namespace}.public.pem"
+  file_permission   = "0400"
 }
 
 resource "local_file" "public_key_in_openssh_format" {
-  sensitive_content    = tls_private_key.key_pair.public_key_openssh
-  filename             = "${path.module}/${local.namespace}.public.openssh"
-  file_permission      = "0400"
+  sensitive_content = tls_private_key.key_pair.public_key_openssh
+  filename          = "${path.module}/${local.namespace}.public.openssh"
+  file_permission   = "0400"
 }
