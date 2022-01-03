0c9ea70b26bb1c42ccf61f1d0035a2cc8e991696
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-02 20:52:47 -0500

    add pods b and c

--- milestone2/pods.tf
+++ milestone2/pods.tf
@@ -10,30 +10,26 @@ module "pod_a" {
   vpc_id                         = aws_vpc.vpc.id
 }
 
-/*
 module "pod_b" {
-  source                 = "./modules/pod"
-  availability_zone_name = data.aws_availability_zone.b.name
+  source                         = "./modules/pod"
+  availability_zone_name         = data.aws_availability_zone.b.name
   cidr_block_allowed_inbound_ssh = "${module.myip.my_public_ip}/32"
-  cidr_block_app         = "172.16.5.0/24"
-  cidr_block_db          = "172.16.9.0/24"
-  cidr_block_public      = "172.16.2.0/24"
-  key_pair_id            = aws_key_pair.key_pair.key_pair_id
-  namespace              = local.namespace
-  vpc_id                 = aws_vpc.vpc.id
+  cidr_block_app                 = "172.16.5.0/24"
+  cidr_block_db                  = "172.16.9.0/24"
+  cidr_block_public              = "172.16.2.0/24"
+  key_pair_id                    = aws_key_pair.key_pair.key_pair_id
+  namespace                      = local.namespace
+  vpc_id                         = aws_vpc.vpc.id
 }
-*/
 
-/*
 module "pod_c" {
-  source                 = "./modules/pod"
-  availability_zone_name = data.aws_availability_zone.c.name
+  source                         = "./modules/pod"
+  availability_zone_name         = data.aws_availability_zone.c.name
   cidr_block_allowed_inbound_ssh = "${module.myip.my_public_ip}/32"
-  cidr_block_app         = "172.16.6.0/24"
-  cidr_block_db          = "172.16.10.0/24"
-  cidr_block_public      = "172.16.3.0/24"
-  key_pair_id            = aws_key_pair.key_pair.key_pair_id
-  namespace              = local.namespace
-  vpc_id                 = aws_vpc.vpc.id
+  cidr_block_app                 = "172.16.6.0/24"
+  cidr_block_db                  = "172.16.10.0/24"
+  cidr_block_public              = "172.16.3.0/24"
+  key_pair_id                    = aws_key_pair.key_pair.key_pair_id
+  namespace                      = local.namespace
+  vpc_id                         = aws_vpc.vpc.id
 }
-*/
