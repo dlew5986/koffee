c83acea51ea03684a5f2c2b66339d7e9f20d7347
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-03 21:42:45 -0500

    add network_3_tier module

---
+++ milestone3/modules/network_3_tier/variables.tf
@@ -0,0 +1,15 @@
+variable "namespace" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = number
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "source_cidr_block" {
+  type = string
+}
