2bdc7480a85741bb645f344029fec23f4b86fe91
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-03 21:42:04 -0500

    add myip module to milestone3

---
+++ milestone3/modules/myip/ip_validation/main.tf
@@ -0,0 +1,11 @@
+variable "input" {
+  type = string
+  validation {
+    condition     = can(regex("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", var.input))
+    error_message = "Invalid IP address."
+  }
+}
+
+output "output" {
+  value = var.input
+}
