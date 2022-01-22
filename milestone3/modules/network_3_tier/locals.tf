c2ae0500c31fc2a9a9a4f9584cfa636e3856230f
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-21 23:08:44 -0500

    add bastion

--- milestone3/modules/network_3_tier/locals.tf
+++ milestone3/modules/network_3_tier/locals.tf
@@ -1,10 +1,12 @@
 locals {
   name_eip                 = "${var.namespace}_eip"
   name_igw                 = "${var.namespace}_igw"
+  name_instance_bastion    = "${var.namespace}_bastion"
   name_key_pair            = "${var.namespace}_key_pair"
   name_nat_gw              = "${var.namespace}_natgw"
   name_route_table_private = "${var.namespace}_private"
   name_route_table_public  = "${var.namespace}_public"
+  name_sg_bastion          = "${var.namespace}_sg_bastion"
   name_subnet_public       = "${var.namespace}_public"
   name_subnet_app          = "${var.namespace}_app"
   name_subnet_database     = "${var.namespace}_database"
