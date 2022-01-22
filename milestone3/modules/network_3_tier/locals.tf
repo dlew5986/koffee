4f880db046966c8b40bf57c114d8419d964a1eb1
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-21 21:09:45 -0500

    add nat gateways

--- milestone3/modules/network_3_tier/locals.tf
+++ milestone3/modules/network_3_tier/locals.tf
@@ -1,8 +1,10 @@
 locals {
+  name_eip             = "${var.namespace}_eip"
+  name_igw             = "${var.namespace}_igw"
+  name_nat_gw          = "${var.namespace}_natgw"
   name_subnet_public   = "${var.namespace}_public"
   name_subnet_app      = "${var.namespace}_app"
   name_subnet_database = "${var.namespace}_database"
-  name_igw             = "${var.namespace}_igw"
   name_vpc             = "${var.namespace}_vpc"
 
   cidr_block_public   = cidrsubnet(var.vpc_cidr_block, 4, 0)
