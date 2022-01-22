5ce6745afd4b0e1d9c9acd994dadfa69d036da51
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2022-01-21 21:56:58 -0500

    add route tables and subnet associations

--- milestone3/modules/network_3_tier/locals.tf
+++ milestone3/modules/network_3_tier/locals.tf
@@ -1,11 +1,13 @@
 locals {
-  name_eip             = "${var.namespace}_eip"
-  name_igw             = "${var.namespace}_igw"
-  name_nat_gw          = "${var.namespace}_natgw"
-  name_subnet_public   = "${var.namespace}_public"
-  name_subnet_app      = "${var.namespace}_app"
-  name_subnet_database = "${var.namespace}_database"
-  name_vpc             = "${var.namespace}_vpc"
+  name_eip                 = "${var.namespace}_eip"
+  name_igw                 = "${var.namespace}_igw"
+  name_nat_gw              = "${var.namespace}_natgw"
+  name_route_table_private = "${var.namespace}_private"
+  name_route_table_public  = "${var.namespace}_public"
+  name_subnet_public       = "${var.namespace}_public"
+  name_subnet_app          = "${var.namespace}_app"
+  name_subnet_database     = "${var.namespace}_database"
+  name_vpc                 = "${var.namespace}_vpc"
 
   cidr_block_public   = cidrsubnet(var.vpc_cidr_block, 4, 0)
   cidr_block_app      = cidrsubnet(var.vpc_cidr_block, 4, 1)
